e400ffdfe3f9684b26b17d656bd596b17c89b29f
developer2 <domayun@gmail.com>
2024-11-16 08:38:56 +0900

    Add sum function in ctrl.py

--- ctrl.py
+++ ctrl.py
@@ -9,6 +9,9 @@ class Control:
     def calculate(self):
         pass
 
+    def sum(sefl, a, b):
+        return a + b
+
     def connectSignals(self):
         self.view.btn2.clicked.connect(self.view.clearMessage) # 버튼 클릭 시 clearMessage 메소드 호출
         self.view.btn1.clicked.connect(self.view.activateMessage) # 버튼 클릭 시 activateMessage 메소드 호출
