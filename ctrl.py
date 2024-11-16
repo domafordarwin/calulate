69e158e30db0279f49e38effb61b8cf917b411db
developer2 <domayun@gmail.com>
2024-11-16 09:34:31 +0900

    Fixed calculate function error

--- ctrl.py
+++ ctrl.py
@@ -7,14 +7,23 @@ class Control:
         self.connectSignals()
 
     def calculate(self):
-        pass
+        num1 = float(self.view.le1.text())
+        num2 = float(self.view.le2.text())
+        operator = self.view.cb.currentText()
+
+        if operator == '+':
+            result = f'{num1} + {num2} = {self.sum(num1, num2)}'
+        
+        else:
+            result = 'Calculation Error'
+
+        return result
 
     def sum(sefl, a, b):
-        try:
-            return str(a + b)
-        except:
-            return 'Calculation Error'
+        return a + b
+       
 
     def connectSignals(self):
         self.view.btn2.clicked.connect(self.view.clearMessage) # 버튼 클릭 시 clearMessage 메소드 호출
-        self.view.btn1.clicked.connect(self.view.setDisplay) # 버튼 클릭 시 activateMessage 메소드 호출
+        self.view.btn1.clicked.connect(lambda:\
+            self.view.setDisplay(str(self.calculate()))) # 버튼 클릭 시 activateMessage 메소드 호출
